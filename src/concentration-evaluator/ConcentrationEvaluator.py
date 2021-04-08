484a30347618fc91f9cd935254a79bf17d0ad3ce
Florian Bufler <florian.bufler@gmx.de>
2021-04-08 22:14:25 +0200

    Implemented base structure

---
+++ src/concentration-evaluator/ConcentrationEvaluator.py
@@ -0,0 +1,9 @@
+class ConcentrationEvaluator:
+    '''
+        Input: logged User Data (time, data)
+        Some Math
+        Output: concentrationValue
+        tbd: make nice docstring
+    '''
+    def __init__(self, data):
+        pass
---
+++ src/user-input-logger/UserInputLogger.py
@@ -0,0 +1,17 @@
+class UserInputLogger:
+    '''
+        A Logger which logs the User Input based on several options
+        tbd:
+            make nice docstring
+            implementation
+            log to file or object?
+            ....
+    '''
+    def __init__(self):
+        pass
+    
+    def setOptions(self, options):
+        pass
+    
+    def start(self):
+        pass
