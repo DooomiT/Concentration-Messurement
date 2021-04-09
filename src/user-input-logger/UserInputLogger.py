6e5ede3d1943633aa97da9534b03aec6a82f77e9
Florian Bufler <florian.bufler@gmx.de>
2021-04-09 10:07:15 +0200

    Partially implemented

--- src/user-input-logger/UserInputLogger.py
+++ src/user-input-logger/UserInputLogger.py
@@ -1,3 +1,19 @@
+from pynput.keyboard import Key, Listener
+from pynput import keyboard
+import logging
+import os
+import time
+from threading import Thread
+
+# CONST
+LOG_DELAY_S = 10
+LOG_DIR = "{}{}".format(os.getcwd(), "\\logs")
+LOG_PATH = "{}\\{}".format(LOG_DIR, "UserInputLogger.log")
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+logging.basicConfig(filename = (LOG_PATH), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
 class UserInputLogger:
     '''
         A Logger which logs the User Input based on several options
@@ -8,10 +24,41 @@ class UserInputLogger:
             ....
     '''
     def __init__(self):
+        logging.info("initialised UserInputLogger")
         pass
     
     def setOptions(self, options):
+        logging.info("set options: {}".format(options))
+        # TODO: implement
         pass
     
+    def on_press(self, key):
+        return str(key)
+    
+    def run(self):
+        while(True):
+            end_time = time.perf_counter() + LOG_DELAY_S
+            keys = []
+            while time.perf_counter() < end_time:   
+                with keyboard.Events() as events:
+                    event = events.get(LOG_DELAY_S)
+                    if isinstance(event, keyboard.Events.Press):
+                        keys.append("{}".format(event.key))
+            logging.info("{} keys were pressed".format(len(keys)))
+            # TODO: Pass keys somehow to the evaluation
+                     
     def start(self):
-        pass
+        logging.info("started UserInputLogger")
+        thread = Thread(target = self.run)
+        thread.start()
+        return thread
+    
+   
+    
+    
+# TEST - TBR               
+uil = UserInputLogger()
+thread = uil.start()
+time.sleep(10)
+thread.join()
+            
